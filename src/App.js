f0b65bb42e2ad6eee193722079fb19d732b463da
brown-chicken1 <hanchen.sun.2019@economics.smu.edu.sg>
2021-10-31 15:52:46 +0800

    testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 // import data from './data';
 import { Layout } from 'antd';
 import View1 from './views/View1';
+import View2 from './views/View2';
 
 import * as d3 from "d3";
 import "./App.css";
@@ -12,6 +13,10 @@ const { Content } = Layout;
 function App() {
   const [data, setData] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
+
+  console.log(data);
+    console.log(View1);
+    console.log(View2);
 
   React.useEffect(() => {
     d3.csv("/athlete_events.csv").then((d) => {
@@ -24,10 +29,13 @@ function App() {
   return (
     <div>
       <div className='title'>Visualising Olympics Data</div>
-      <Layout style={{ height: 920 }}>
+      <Layout style={{ height: 600 }}>
           <Layout>
             <Content style={{ height: 200, width: 300 }}>
               <View1 data={data}/>
+            </Content>
+              <Content style={{ height: 200, width: 300 }}>
+              <View2 data={data}/>
             </Content>
           </Layout>
       </Layout>
